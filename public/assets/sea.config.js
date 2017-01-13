6cda4b6052d259d6adf06170c68ba6e4e32b2149
gsjohn <sz.gs.john@gmail.com>
2017-01-13 20:43:54 +0800

    add editor i18n

--- public/assets/sea.config.js
+++ public/assets/sea.config.js
@@ -5,5 +5,6 @@ seajs.config({
     alias: {
         "datatablePipeline": "plugins/datatables/js/pipeline.js",
         "datatableZh": "plugins/datatables/js/zh_CN.js",
+        "i18n": "plugins/datatables/extensions/Buttons/js/i18n.js",
     }
 });
